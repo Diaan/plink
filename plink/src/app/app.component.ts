87ab97b39a42fc1a107ec82d9d75cf23a81ea480
Diaan <info@dianabroeders.nl>
2017-06-25 22:40:06 +0200

    added blip object

--- plink/src/app/app.component.ts
+++ plink/src/app/app.component.ts
@@ -8,5 +8,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bla';
   looplength = 30;
-  blips = new Array(10);
+  blips = new Array(0);
+  constructor(){
+    let index;
+    for (index = 0; index < 10; index++) {
+      this.blips.push(new Blip())
+    }
+  }
+  toggleBlip(blip) {
+    blip.toggle();
+    console.log(blip);
+  }
+}
+
+class Blip{
+  on = false;
+  toggle(){
+    this.on = !this.on;
+  }
+  /*
+  sound = "";
+  constructor(sound){
+    this.sound = sound;
+  }
+
+
+  play(){
+    console.log("play",this.sound);
+  }
+  */
 }
