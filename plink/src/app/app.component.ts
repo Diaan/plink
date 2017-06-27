a705104b380cf0ce8ba00d6a8e5db30cb939ce09
Diaan <info@dianabroeders.nl>
2017-06-27 07:18:36 +0200

    start on animation

--- plink/src/app/app.component.ts
+++ plink/src/app/app.component.ts
@@ -7,17 +7,70 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bla';
-  looplength = 30;
-  blips = new Array(0);
+  looplength = 5;
+  nrOfTracks = 1;
+  nrOfBlips = 2;
+  track = {};
+  private playInterval;
+  public playHead;
+
   constructor(){
-    let index;
-    for (index = 0; index < 10; index++) {
-      this.blips.push(new Blip())
-    }
+    this.track = new Track(this.nrOfBlips);
+    this.playHead = new PlayHead(this.looplength*1000/this.nrOfBlips);
   }
+
   toggleBlip(blip) {
     blip.toggle();
-    console.log(blip);
+  }
+  playPlink(){
+    if(typeof this.playInterval=='undefined'){
+      this.playHead.move(this.nrOfBlips);
+      this.playInterval = setInterval(function(){
+        this.playHead.move(this.nrOfBlips);
+      }.bind(this),this.looplength*1000/this.nrOfBlips)
+    }else{
+      console.log('already playing');
+    }
+  }
+  stopPlink(){
+    clearInterval(this.playInterval);
+  }
+  pausePlink(){
+
+  }
+}
+
+class PlayHead{
+  private position;
+  private animationLength;
+  private animLen;
+  constructor(animationLength){
+    this.position = 0;
+    this.animLen = animationLength;
+    this.animationLength = animationLength+'ms';
+  }
+  move(nrOfBlips){
+    console.log(this.position,this.animationLength)
+    /*if(this.position==100){
+      this.animationLength = '0s';
+      this.position = 0;
+    }*/
+    if(this.position+100/nrOfBlips>=100){
+     this.position = 0;
+      this.animationLength = this.animLen+'ms';
+    }else{
+      this.position += 100/nrOfBlips;
+      //this.animationLength = this.animLen+'ms';
+    }
+  }
+}
+
+class Track{
+  blips = new Array(0);
+  constructor(nrOfBlips){
+    for (let index = 0; index < nrOfBlips; index++) {
+      this.blips.push(new Blip())
+    }
   }
 }
 
